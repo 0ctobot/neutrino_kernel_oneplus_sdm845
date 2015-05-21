8448fc42530c9fb9bc3c5ce1c1b0b05975c8f48f
Vinayak Menon <vinmenon@codeaurora.org>
2015-05-21 20:44:14 +0530

    lowmemorykiller: use for_each_thread instead of buggy while_each_thread
    Couple of cases were reported few months ago, where the cpu was blocked
    on the following call stack for /seconds/ after which the watchdog fires.
    
    test_task_flag(p = 0xE14ABF00, ?)
    lowmem_shrink(?, sc = 0xD7A03C04)
    shrink_slab(shrink = 0xD7A03C04, nr_pages_scanned = 0, lru_pages = 120)
    try_to_free_pages(zonelist = 0xC1116440, ?, ?, ?)
    __alloc_pages_nodemask(?, order = 0, ?, nodemask = 0x0)
    __do_page_cache_readahead(mapping = 0xEB819364, filp = 0xCC16DC00, offset =
    ra_submit(?, ?, ?)
    filemap_fault(vma = 0xC105D240, vmf = 0xD7A03DC8)
    
    There weren't any dumps to analyse the case, but this can be a possible
    reason. while_each_thread is known to be buggy and can result in the
    function looping forever if the task exits, even when protected with
    rcu_read_lock. Use for_each_thread instead.
    
    More details on the problems with while_each_thread can be found
    at https://lkml.org/lkml/2013/12/2/320
    
    Change-Id: I5eb6e4b463f81142a2a7824db389201357432ec7
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -83,32 +83,32 @@ static unsigned long lowmem_count(struct shrinker *s,
 
 static int test_task_flag(struct task_struct *p, int flag)
 {
-	struct task_struct *t = p;
+	struct task_struct *t;
 
-	do {
+	for_each_thread(p, t) {
 		task_lock(t);
 		if (test_tsk_thread_flag(t, flag)) {
 			task_unlock(t);
 			return 1;
 		}
 		task_unlock(t);
-	} while_each_thread(p, t);
+	}
 
 	return 0;
 }
 
 static int test_task_lmk_waiting(struct task_struct *p)
 {
-	struct task_struct *t = p;
+	struct task_struct *t;
 
-	do {
+	for_each_thread(p, t) {
 		task_lock(t);
 		if (task_lmk_waiting(t)) {
 			task_unlock(t);
 			return 1;
 		}
 		task_unlock(t);
-	} while_each_thread(p, t);
+	}
 
 	return 0;
 }
