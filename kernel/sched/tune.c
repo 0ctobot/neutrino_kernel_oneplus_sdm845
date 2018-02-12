48fbffca7d3540683e9475192719f9ce13995807
Patrick Bellasi <patrick.bellasi@arm.com>
2018-02-12 16:04:35 +0000

    Reapply eafebca ("ANDROID: sched/tune: cleanup schedtune_boostgroup_{init,release}")
    This update the definition of these two methods to make more clear their
    role.
    
    In this refactored version, the slow path entry functions:
       schedtune_css_alloc()
       schedtune_css_free()
    are in charge just to allocate and release the memory used to track
    schedtune boost groups.
    
    These two functions rely respectively on:
       schedtune_boostgroup_init()
       schedtune_boostgroup_release()
    for everything related to setting up data structures and properly
    initializing them.
    
    Change-Id: I9336102b5c6a6b5726fd466e99b7d6b28d38f455
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>
    (cherry picked from commit eafebcabecd08e7a7e71481bcda6d6f788622495)
    
    (Resolved merge conflicts with Dynamic Stune)
    Signed-off-by: RenderBroken <zkennedy87@gmail.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -1002,20 +1002,17 @@ static struct cftype files[] = {
 	{ }	/* terminate */
 };
 
-static int
-schedtune_boostgroup_init(struct schedtune *st)
+static void
+schedtune_boostgroup_init(struct schedtune *st, int idx)
 {
 	struct boost_groups *bg;
 	int cpu;
 
-	/* Keep track of allocated boost groups */
-	allocated_group[st->idx] = st;
-
-	/* Initialize the per CPU boost groups */
+	/* Initialize per CPUs boost group support */
 	for_each_possible_cpu(cpu) {
 		bg = &per_cpu(cpu_boost_groups, cpu);
-		bg->group[st->idx].boost = 0;
-		bg->group[st->idx].tasks = 0;
+		bg->group[idx].boost = 0;
+		bg->group[idx].tasks = 0;
 		bg->group[st->idx].ts = 0;
 	}
 
@@ -1023,7 +1020,9 @@ schedtune_boostgroup_init(struct schedtune *st)
 	boost_slots_init(st);
 #endif // CONFIG_DYNAMIC_STUNE_BOOST
 
-	return 0;
+	/* Keep track of allocated boost groups */
+	allocated_group[idx] = st;
+	st->idx = idx;
 }
 
 static struct cgroup_subsys_state *
@@ -1056,15 +1055,11 @@ schedtune_css_alloc(struct cgroup_subsys_state *parent_css)
 		goto out;
 
 	/* Initialize per CPUs boost group support */
-	st->idx = idx;
 	init_sched_boost(st);
-	if (schedtune_boostgroup_init(st))
-		goto release;
+	schedtune_boostgroup_init(st, idx);
 
 	return &st->css;
 
-release:
-	kfree(st);
 out:
 	return ERR_PTR(-ENOMEM);
 }
@@ -1072,12 +1067,19 @@ out:
 static void
 schedtune_boostgroup_release(struct schedtune *st)
 {
+	struct boost_groups *bg;
+	int cpu;
+
+	/* Reset per CPUs boost group support */
+	for_each_possible_cpu(cpu) {
+		bg = &per_cpu(cpu_boost_groups, cpu);
+		bg->group[st->idx].boost = 0;
+	}
+
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	/* Free dynamic boost slots */
 	boost_slots_release(st);
 #endif // CONFIG_DYNAMIC_STUNE_BOOST
-	/* Reset this boost group */
-	schedtune_boostgroup_update(st->idx, 0);
 
 	/* Keep track of allocated boost groups */
 	allocated_group[st->idx] = NULL;
@@ -1088,6 +1090,7 @@ schedtune_css_free(struct cgroup_subsys_state *css)
 {
 	struct schedtune *st = css_st(css);
 
+	/* Release per CPUs boost group support */
 	schedtune_boostgroup_release(st);
 	kfree(st);
 }
