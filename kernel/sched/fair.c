2920a5453f184da2cf7919ff40d48857ebd988cf
Rick Yiu <rickyiu@google.com>
2018-05-18 11:54:10 +0800

    sched/fair: remove sync logic from select_energy_cpu_brute
    This patch is originated from wahoo-4.4 kernel,
    commit 9767a33691cb ("Move sync logic out of select_energy_cpu_brute")
    but only part of it, since sync logic is already under
    select_task_rq_fair().
    
    bug: 78790904
    Change-Id: I22e27cc3b6218e4eff10847af4d9cce93a99cf38
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7424,7 +7424,7 @@ enum fastpaths {
 	PREV_CPU_BIAS,
 };
 
-static int select_energy_cpu_brute(struct task_struct *p, int prev_cpu, int sync)
+static int select_energy_cpu_brute(struct task_struct *p, int prev_cpu)
 {
 	bool boosted, prefer_idle;
 	struct sched_domain *sd;
@@ -7461,21 +7461,6 @@ static int select_energy_cpu_brute(struct task_struct *p, int prev_cpu, int sync
 				  sched_boost_policy() != SCHED_BOOST_NONE :
 				  false;
 	fbt_env.avoid_prev_cpu = false;
-
-	if (prefer_idle || fbt_env.need_idle)
-		sync = 0;
-
-	if (sysctl_sched_sync_hint_enable && sync) {
-		int cpu = smp_processor_id();
-
-		if (bias_to_waker_cpu(p, cpu, rtg_target)) {
-			schedstat_inc(p->se.statistics.nr_wakeups_secb_sync);
-			schedstat_inc(this_rq()->eas_stats.secb_sync);
-			target_cpu = cpu;
-			fastpath = SYNC_WAKEUP;
-			goto out;
-		}
-	}
 
 	if (bias_to_prev_cpu(p, rtg_target)) {
 		target_cpu = prev_cpu;
@@ -7571,7 +7556,7 @@ static int select_energy_cpu_brute(struct task_struct *p, int prev_cpu, int sync
 unlock:
 	rcu_read_unlock();
 out:
-	trace_sched_task_util(p, next_cpu, backup_cpu, target_cpu, sync,
+	trace_sched_task_util(p, next_cpu, backup_cpu, target_cpu,
 			      fbt_env.need_idle, fastpath,
 			      fbt_env.placement_boost, rtg_target ?
 			      cpumask_first(rtg_target) : -1, start_t);
@@ -7616,7 +7601,7 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 	}
 
 	if (energy_aware())
-		return select_energy_cpu_brute(p, prev_cpu, sync);
+		return select_energy_cpu_brute(p, prev_cpu);
 
 	rcu_read_lock();
 	for_each_domain(cpu, tmp) {
@@ -11969,7 +11954,7 @@ void check_for_migration(struct rq *rq, struct task_struct *p)
 
 		raw_spin_lock(&migration_lock);
 		rcu_read_lock();
-		new_cpu = select_energy_cpu_brute(p, cpu, 0);
+		new_cpu = select_energy_cpu_brute(p, cpu);
 		rcu_read_unlock();
 		if (capacity_orig_of(new_cpu) > capacity_orig_of(cpu)) {
 			active_balance = kick_active_balance(rq, p, new_cpu);
