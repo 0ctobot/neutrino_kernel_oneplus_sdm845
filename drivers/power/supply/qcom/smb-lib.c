de533d78bd69994e6eda3aae5b25eb94bdc155da
Nathan Chancellor <natechancellor@gmail.com>
2018-05-19 17:38:37 -0700

    power: smb-lib: Remove duplicate const specifier
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -285,7 +285,7 @@ enum {
 	MAX_TYPES
 };
 
-static const struct apsd_result const smblib_apsd_results[] = {
+static const struct apsd_result smblib_apsd_results[] = {
 	[UNKNOWN] = {
 		.name	= "UNKNOWN",
 		.bit	= 0,
