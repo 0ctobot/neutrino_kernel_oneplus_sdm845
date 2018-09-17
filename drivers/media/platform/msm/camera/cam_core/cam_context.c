7146f6be37f9a533348eaa06bd22c1206477be18
Karthik Anantha Ram <kartanan@codeaurora.org>
2018-09-17 16:04:42 -0700

    msm: camera: Modify shutdown sequence in KMD
    This change ensures subdevices close before the video device
    cleans up. Memory manager is currently associated with request
    manager, with this change we ensure all subdevices cleanup prior
    to cleaning up the request/memory manager.
    
    Signed-off-by: Karthik Anantha Ram <kartanan@codeaurora.org>
    Change-Id: Ia6b715025ee940ad14e4b4bbe8ed747d895fb342
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/media/platform/msm/camera/cam_core/cam_context.c
+++ drivers/media/platform/msm/camera/cam_core/cam_context.c
@@ -41,6 +41,7 @@ int cam_context_shutdown(struct cam_context *ctx)
 {
 	int rc = 0;
 
+	mutex_lock(&ctx->ctx_mutex);
 	if (ctx->state_machine[ctx->state].ioctl_ops.stop_dev) {
 		rc = ctx->state_machine[ctx->state].ioctl_ops.stop_dev(
 			ctx, NULL);
@@ -53,6 +54,7 @@ int cam_context_shutdown(struct cam_context *ctx)
 		if (rc < 0)
 			CAM_ERR(CAM_CORE, "Error while dev release %d", rc);
 	}
+	mutex_unlock(&ctx->ctx_mutex);
 
 	return rc;
 }
