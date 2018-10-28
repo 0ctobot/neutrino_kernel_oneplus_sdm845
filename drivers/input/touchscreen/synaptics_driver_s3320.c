a37d81a1dcd3d259389248a42570abb67e36cfea
Park Ju Hyung <qkrwngud825@gmail.com>
2018-10-29 02:59:45 +0900

    s3320: call pm_qos_add_request() earlier
    s3320's IRQ handler gets called during probe and pm_qos_update_request()
    gets called with NULL object inside.
    
    Fix this by calling pm_qos_add_request() earlier during probe.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -6187,6 +6187,9 @@ static int synaptics_ts_probe(struct i2c_client *client,
 		goto err_alloc_data_failed;
 	}
 
+	pm_qos_add_request(&ts->pm_qos_req, PM_QOS_CPU_DMA_LATENCY,
+		PM_QOS_DEFAULT_VALUE);
+
 	ts->client = client;
 	i2c_set_clientdata(client, ts);
 	ts->dev = &client->dev;
@@ -6459,9 +6462,6 @@ static int synaptics_ts_probe(struct i2c_client *client,
 #endif
 	init_synaptics_proc();
 
-	pm_qos_add_request(&ts->pm_qos_req, PM_QOS_CPU_DMA_LATENCY,
-		PM_QOS_DEFAULT_VALUE);
-
 	TPDTM_DMESG("synaptics_ts_probe 3203: normal end\n");
 
 	return 0;
@@ -6488,6 +6488,7 @@ static int synaptics_ts_probe(struct i2c_client *client,
 	kfree(rmi4_data_s3706);
 	rmi4_data_s3706 = NULL;
 	ts_g = NULL;
+	pm_qos_remove_request(&ts->pm_qos_req);
 	TPD_ERR("synaptics_ts_probe: not normal end\n");
 	return ret;
 }
