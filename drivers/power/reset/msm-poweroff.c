e0faafe2e2e9fd9e1ddc2e994e9890d70a939324
Adam W. Willis <return.of.octobot@gmail.com>
2019-02-10 21:23:17 -0500

    msm-poweroff: Remove unused variables
    In 9d77fb1 ("msm-poweroff: always preserve PMIC reset status"), we
    eliminated the primary function of the need_warm_reset and
    get_dload_mode variables - to detect whether or not download mode is
    enabled and determine if a warm reset should be performed. Because we
    are now forcing a warm reset regardless of CONFIG_QCOM_DLOAD_MODE
    status, the necessity of this check has been nullified.
    
    This silences the following compiler warnings:
    
    ../drivers/power/reset/msm-poweroff.c:285:7: warning: unused variable
          'need_warm_reset' [-Wunused-variable]
            bool need_warm_reset = false;
                 ^
    ../drivers/power/reset/msm-poweroff.c:152:13: warning: unused function
          'get_dload_mode' [-Wunused-function]
    static bool get_dload_mode(void)
                ^
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -149,11 +149,6 @@ static void set_dload_mode(int on)
 	dload_mode_enabled = on;
 }
 
-static bool get_dload_mode(void)
-{
-	return dload_mode_enabled;
-}
-
 static void enable_emergency_dload_mode(void)
 {
 	int ret;
@@ -211,11 +206,6 @@ static void set_dload_mode(int on)
 static void enable_emergency_dload_mode(void)
 {
 	pr_err("dload mode is not enabled on target\n");
-}
-
-static bool get_dload_mode(void)
-{
-	return false;
 }
 #endif
 
@@ -282,7 +272,6 @@ static void halt_spmi_pmic_arbiter(void)
 
 static void msm_restart_prepare(const char *cmd)
 {
-	bool need_warm_reset = false;
 #ifdef CONFIG_QCOM_DLOAD_MODE
 	/* Write download mode flags if we're panic'ing
 	 * Write download mode flags if restart_mode says so
