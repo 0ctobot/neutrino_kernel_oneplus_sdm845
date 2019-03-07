3d23f044488b106682d4fbd12d8d4c911cfe5902
0ctobot <return.of.octobot@gmail.com>
2019-03-06 20:55:26 -0500

    cpu-boost: Hardcode dynamic_stune_boost
    Reduce the need for ramdisk modifications where possible.
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -45,7 +45,7 @@ module_param(sched_boost_on_input, uint, 0644);
 static bool sched_boost_active;
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
-static int dynamic_stune_boost;
+static int dynamic_stune_boost = 15;
 module_param(dynamic_stune_boost, uint, 0644);
 static bool stune_boost_active;
 static int boost_slot;
