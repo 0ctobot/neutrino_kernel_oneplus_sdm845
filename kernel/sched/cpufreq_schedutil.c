b2bcc65a1ae7703d49875f0c9f8bde0ede67adce
Park Ju Hyung <qkrwngud825@gmail.com>
2019-04-02 17:44:17 +0900

    treewide: hybrid merge some bits from aura and violet
    TODO: Revert this later and cherry-pick appropriate commits when msm-4.14 kernel drops
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    [@0ctobot: This reverts commit 55093b8 ("cpufreq: schedutil: Hardcode iowait_boost_enable")]
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -33,7 +33,6 @@ struct sugov_tunables {
 	unsigned int hispeed_load;
 	unsigned int hispeed_freq;
 	bool pl;
-	bool iowait_boost_enable;
 };
 
 struct sugov_policy {
@@ -166,7 +165,7 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 
 	if (policy->fast_switch_enabled) {
 		next_freq = cpufreq_driver_fast_switch(policy, next_freq);
-		if (!next_freq)
+		if (next_freq == CPUFREQ_ENTRY_INVALID)
 			return;
 
 		policy->cur = next_freq;
@@ -234,11 +233,6 @@ static void sugov_get_util(unsigned long *util, unsigned long *max, int cpu)
 
 static void sugov_set_iowait_boost(struct sugov_cpu *sg_cpu, u64 time)
 {
-	struct sugov_policy *sg_policy = sg_cpu->sg_policy;
-
-	if (!sg_policy->tunables->iowait_boost_enable)
-		return;
-
 	/* Clear iowait_boost if the CPU apprears to have been idle. */
 	if (sg_cpu->iowait_boost) {
 		s64 delta_ns = time - sg_cpu->last_update;
@@ -735,27 +729,6 @@ static ssize_t pl_store(struct gov_attr_set *attr_set, const char *buf,
 
 	if (kstrtobool(buf, &tunables->pl))
 		return -EINVAL;
-	return count;
-}
-
-static ssize_t iowait_boost_enable_show(struct gov_attr_set *attr_set,
-					char *buf)
-{
-	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
-
-	return sprintf(buf, "%u\n", tunables->iowait_boost_enable);
-}
-
-static ssize_t iowait_boost_enable_store(struct gov_attr_set *attr_set,
-					 const char *buf, size_t count)
-{
-	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
-	bool enable;
-
-	if (kstrtobool(buf, &enable))
-		return -EINVAL;
-
-	tunables->iowait_boost_enable = enable;
 
 	return count;
 }
@@ -765,7 +738,6 @@ static struct governor_attr down_rate_limit_us = __ATTR_RW(down_rate_limit_us);
 static struct governor_attr hispeed_load = __ATTR_RW(hispeed_load);
 static struct governor_attr hispeed_freq = __ATTR_RW(hispeed_freq);
 static struct governor_attr pl = __ATTR_RW(pl);
-static struct governor_attr iowait_boost_enable = __ATTR_RW(iowait_boost_enable);
 
 static struct attribute *sugov_attributes[] = {
 	&up_rate_limit_us.attr,
@@ -773,7 +745,6 @@ static struct attribute *sugov_attributes[] = {
 	&hispeed_load.attr,
 	&hispeed_freq.attr,
 	&pl.attr,
-	&iowait_boost_enable.attr,
 	NULL
 };
 
@@ -979,8 +950,6 @@ static int sugov_init(struct cpufreq_policy *policy)
 	/* Hard-code some sane rate-limit values */
 	tunables->up_rate_limit_us = 10000;
 	tunables->down_rate_limit_us = 20000;
-
-	tunables->iowait_boost_enable = true;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
